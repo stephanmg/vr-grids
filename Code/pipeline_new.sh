5272b568722f69c6cc3acb4e33b67a04e5c5ec54
stephan <stephan@syntaktischer-zucker.de>
2021-04-28 07:50:41 +0200

    Refactoring.

--- Code/pipeline_new.sh
+++ Code/pipeline_new.sh
@@ -8,21 +8,21 @@
 INFLATIONS=1
 REFINEMENTS=2
 SEGMENT_LENGTH=6
-FILENAME=cylinder
+SWC_FILE=cylinder
 BINARY=/home/stephan/Code/git/ug4/bin/ugshell 
-FOLDERNAME=example
+OUTPUT_FOLDER=example
 SCRIPT_3D_VR=test_import_swc_general_var_for_vr_var 
 
 ## create outout folder
-mkdir -p "${FOLDERNAME}"
+mkdir -p "${OUTPUT_FOLDER}"
 
 # create inflations of 3d mesh
 for (( inflation=1; ref < ${INFLATIONS}; ref++)); do 
    # create the 3d coarse mesh
-   $BINARY -call "${SCRIPT_3D_VR}(\"${FILENAME}.swc\", false, 0.3, true, $SEGMENT_LENGTH, 0, true, $inflation, \"user\", $SEGMENT_LENGTH)"
+   $BINARY -call "${SCRIPT_3D_VR}(\"${SWC_FILE}.swc\", false, 0.3, true, $SEGMENT_LENGTH, 0, true, $inflation, \"user\", $SEGMENT_LENGTH)"
 
    # create the 3d refinements and write the 1d meshes (Lua script)
-cat << EOF > ${FOLDERNAME}/geom.lua
+cat << EOF > ${OUTPUT_FOLDER}/geom.lua
 -- init ug
 ug_load_script("ug_util.lua")
 ug_load_script("util/load_balancing_util.lua")
@@ -37,7 +37,7 @@ LoadDomain(dom, "after_selecting_boundary_elements_with_projector.ugx")
 axialMarker = NeuriteAxialRefinementMarker(dom)
 refiner = HangingNodeDomainRefiner(dom)
 for ref=0, $((REFINEMENTS-1)) do
-   SaveGridLevelToFile(dom:grid(), dom:subset_handler(), ref, "${FOLDERNAME}/${FILENAME%*.swc}_3d_x${inflation}_ref_" .. ref .. ".ugx")
+   SaveGridLevelToFile(dom:grid(), dom:subset_handler(), ref, "${OUTPUT_FOLDER}/${SWC_FILE%*.swc}_3d_x${inflation}_ref_" .. ref .. ".ugx")
    axialMarker:mark(refiner)
    refiner:refine()
 end
@@ -49,59 +49,59 @@ for ref=0, $((REFINEMENTS-1)) do
 end
 EOF
    # execute ugshell
-   $BINARY -ex ${FOLDERNAME}/geom.lua
+   $BINARY -ex ${OUTPUT_FOLDER}/geom.lua
 
    # copy 1d meshes to output folder
    for (( ref=0; ref < ${REFINEMENTS}; ref++)); do
-      cp 1dmesh_${ref}.ugx ${FOLDERNAME}/${FILENAME%*.swc}_1d_ref_${ref}.ugx
+      cp 1dmesh_${ref}.ugx ${OUTPUT_FOLDER}/${SWC_FILE%*.swc}_1d_ref_${ref}.ugx
    done
 
    # remove attachments
    for (( ref=0; ref < ${REFINEMENTS}; ref++)); do
-      sed '/.*vertex_attachment.*/d' "${FOLDERNAME}/${FILENAME%*.swc}_3d_x${inflation}_ref_${ref}.ugx" > "${FOLDERNAME}/${FILENAME%*.swc}_3d_x${inflation}_ref_${ref}_wo_attachments.ugx" 
+      sed '/.*vertex_attachment.*/d' "${OUTPUT_FOLDER}/${SWC_FILE%*.swc}_3d_x${inflation}_ref_${ref}.ugx" > "${FOLDERNAME}/${SWC_FILE%*.swc}_3d_x${inflation}_ref_${ref}_wo_attachments.ugx" 
    done
 done
 
-cat << EOF > ${FOLDERNAME}/MetaInfo.json
+cat << EOF > ${OUTPUT_FOLDER}/MetaInfo.json
 {
     "geom1d" : [
 EOF
 
 for (( ref=0;  ref < ${REFINEMENTS}; ref++)); do
-cat << EOF >> ${FOLDERNAME}/MetaInfo.json
-         { "name" : "${FILENAME}_segLength=${segLength1D}_1d_ref_${ref}.ugx", "description": "1d mesh coarse mesh", "refinement": "$ref",
+cat << EOF >> ${OUTPUT_FOLDER}/MetaInfo.json
+         { "name" : "${SWC_FILE}_segLength=${segLength1D}_1d_ref_${ref}.ugx", "description": "1d mesh coarse mesh", "refinement": "$ref",
            "inflations" : [
 EOF
 for (( inflation=1; ref < $INFLATIONS; ref++)); do 
 inflation=${INFLATIONS[$idx]}
-cat << EOF >> ${FOLDERNAME}/MetaInfo.json
-               { "name" : "${FILENAME}_segLength=${segLength1D}_3d_x${inflation}_ref_${ref}.ugx", "description": "2d surface mesh", "inflation" : "${inflation}" },
+cat << EOF >> ${OUTPUT_FOLDER}/MetaInfo.json
+               { "name" : "${SWC_FILE}_segLength=${segLength1D}_3d_x${inflation}_ref_${ref}.ugx", "description": "2d surface mesh", "inflation" : "${inflation}" },
 EOF
 done
 inflation=$INFLATIONS
-cat << EOF >> ${FOLDERNAME}/MetaInfo.json
-               { "name" : "${FILENAME}_segLength=${segLength1D}_3d_x${inflation}_ref_${ref}.ugx", "description": "2d surface mesh", "inflation" : "${inflation}" }
+cat << EOF >> ${OUTPUT_FOLDER}/MetaInfo.json
+               { "name" : "${SWC_FILE}_segLength=${segLength1D}_3d_x${inflation}_ref_${ref}.ugx", "description": "2d surface mesh", "inflation" : "${inflation}" }
 EOF
 
 lastRef=$REFINEMENTS
 if [ "$lastRef" = "$ref" ]; then
-cat << EOF >> ${FOLDERNAME}/MetaInfo.json
+cat << EOF >> ${OUTPUT_FOLDER}/MetaInfo.json
            ]
          }
 EOF
 else
-cat << EOF >> ${FOLDERNAME}/MetaInfo.json
+cat << EOF >> ${OUTPUT_FOLDER}/MetaInfo.json
            ]
          },
 EOF
 fi
 
 done
-cat << EOF >> ${FOLDERNAME}/MetaInfo.json
+cat << EOF >> ${OUTPUT_FOLDER}/MetaInfo.json
       ]
 }
 EOF
 
-cd ${FOLDERNAME}
-zip -j -x "*_wo_attachments.ugx" -r ${FILENAME}.vrn MetaInfo.json *ugx
+cd ${OUTPUT_FOLDER}
+zip -j -x "*_wo_attachments.ugx" -r ${SWC_FILE}.vrn MetaInfo.json *ugx
 cd ../../
